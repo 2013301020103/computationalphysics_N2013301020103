3917eaa47978ae137d7d4eb39075116c02130a18
2013301020103 <594722742@qq.com>
2016-06-21 23:45:14 +0800

    Update lagrange.py

--- lagrange.py
+++ lagrange.py
@@ -22,12 +22,12 @@ vy2 = diff(y2, t)
 L = m1/2*(vx1**2 + vy1**2) + m2/2*(vx2**2 + vy2**2) - m1*g*y1 - m2*g*y2
 # 拉格朗日方程
 def lagrange_equation(L, v):
-a = L.subs(D(v(t), t), tmp).diff(tmp).subs(tmp, D(v(t), t))
-b = L.subs(D(v(t), t), tmp).subs(v(t), v).diff(v).subs(v, v(t)).subs(tmp,      D(v(t), t))
-c = a.diff(t) - b
-c = c.subs(sublist)
-c = trigsimp(simplify(c))
-c = collect(c, [th1,th2,dth1,dth2,ddth1,ddth2])
-return c
+    a = L.subs(D(v(t), t), tmp).diff(tmp).subs(tmp, D(v(t), t))
+    b = L.subs(D(v(t), t), tmp).subs(v(t), v).diff(v).subs(v, v(t)).subs(tmp,      D(v(t), t))
+    c = a.diff(t) - b
+    c = c.subs(sublist)
+    c = trigsimp(simplify(c))
+    c = collect(c, [th1,th2,dth1,dth2,ddth1,ddth2])
+    return c
 eq1 = lagrange_equation(L, th1)
 eq2 = lagrange_equation(L, th2)
